ffc83d6d54d47b9d48fd625f692c3eaed2a4d147
PutriHabibahDevina <putrihdm13@gmail.com>
2024-11-21 17:17:00 +0700

    Menambahkan nim di kelas DataSiswa

--- app/src/main/java/com/example/praktikum5/model/DataSiswa.kt
+++ app/src/main/java/com/example/praktikum5/model/DataSiswa.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum5.model
 
 data class DataSiswa(
+    val nim: String = "",
     val nama: String = "", //merupakan properti
     val gender: String = "",
     val alamat: String = "", //pake val biar nilainya tetap satu
